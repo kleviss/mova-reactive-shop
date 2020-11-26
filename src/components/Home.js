b26f9191b1504f871b2ad5fcba09e119af6edd64
Klevis Xhyra <klevissxh@gmail.com>
2020-11-26 23:11:55 +0100

    created Homepage to nest all components

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React, { Fragment, useState, useEffect } from "react";
+import Collections from "../components/Collections";
+
+const Home = () => {
+  return(
+    <Fragment>
+      <Collections />
+    </Fragment>
+  )
+}
+
+export default Home;
