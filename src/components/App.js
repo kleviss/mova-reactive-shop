228cdc519c6fd94223a43410c9ac1fbcade671a8
Klevis Xhyra <klevissxh@gmail.com>
2020-11-26 14:13:05 +0100

    project structure - initial commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/App.css";
+import "../styles/index.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>MOVA SHOP</h1>
+    </div>
+  );
+}
+
+export default App;
