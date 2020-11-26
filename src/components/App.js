0de9b92e64b6398e7beeb1d2447ab79a19880611
Klevis Xhyra <klevissxh@gmail.com>
2020-11-26 22:37:57 +0100

    Collection component setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,18 @@
 import React from "react";
-import "../styles/App.css";
-import "../styles/index.css";
 import AppBar from "../components/AppBar";
+import Collections from "../components/Collections";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
 
-function App() {
-  return <AppBar />;
+export default function App() {
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Container disableGutters={true} maxWidth={false}>
+        <AppBar />
+        <Collections />
+      </Container>
+    </React.Fragment>
+  );
 }
-
-export default App;
---
+++ src/components/Collections.js
@@ -0,0 +1,51 @@
+import React, { Fragment, useState, useEffect } from "react";
+import axios from "axios";
+
+const Collections = () => {
+  const [collections, setCollections] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  const url = `https://5m6exoj3o7.execute-api.eu-west-1.amazonaws.com/prod/collections`;
+
+  useEffect(() => {
+    const fetchCollections = async () => {
+      setIsError(false);
+      setIsLoading(true);
+
+      try {
+        const result = await axios(url);
+
+        setCollections(result.data);
+      } catch (error) {
+        setIsError(true);
+      }
+
+      setIsLoading(false);
+    };
+
+    fetchCollections();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <Fragment>
+      {console.log(collections)}
+      {isError && <div>Something went wrong ... Please reload the page</div>}
+
+      {isLoading ? (
+        <div>Getting your favorite apparel...</div>
+      ) : (
+        <div>
+          {" "}
+          {collections.map((tile) => (
+            <h4>{tile.displayName}</h4>
+          ))}
+        </div>
+      )}
+    </Fragment>
+  );
+};
+
+export default Collections;
