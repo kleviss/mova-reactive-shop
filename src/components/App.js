ffc8afac3af648ddfc94f41362db53268d183aa2
Klevis Xhyra <klevissxh@gmail.com>
2020-11-27 00:03:44 +0100

    Routing categry page

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import AppBar from "../components/AppBar";
 import CollectionPage from "../components/CollectionPage";
+import CategoryPage from "../components/CategoryPage";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Container from "@material-ui/core/Container";
 import Home from "./Home";
@@ -16,6 +17,7 @@ export default function App() {
           <Switch>
             <Route path="/" exact component={Home} />
             <Route path="/collection/:id" component={CollectionPage} />
+            <Route path="/category/:id" component={CategoryPage} />
           </Switch>
         </Container>
       </React.Fragment>
