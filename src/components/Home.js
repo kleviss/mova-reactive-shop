fcf9ea3a3dffcb4ef3eacf73ca6730b036a6112d
Klevis Xhyra <klevissxh@gmail.com>
2020-11-27 00:04:41 +0100

    include Categories component to Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
 import React, { Fragment, useState, useEffect } from "react";
 import Collections from "../components/Collections";
+import Categories from "../components/Categories";
 
 const Home = () => {
   return(
     <Fragment>
       <Collections />
+      <Categories />
     </Fragment>
   )
 }
