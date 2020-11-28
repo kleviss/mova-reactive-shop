8ad8099ac8302e08fe0a61ade2c82edd59ee6255
Klevis Xhyra <klevissxh@gmail.com>
2020-11-28 12:34:18 +0100

    accessing TagItem child component tag value and rerender products

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,7 +1,7 @@
 import React, { Fragment, useState, useEffect } from "react";
+import TagItem from "./TagItem.js";
 import Grid from "@material-ui/core/Grid";
 import axios from "axios";
-import Avatar from "@material-ui/core/Avatar";
 import Chip from "@material-ui/core/Chip";
 import FaceIcon from "@material-ui/icons/Face";
 import DoneIcon from "@material-ui/icons/Done";
@@ -11,6 +11,7 @@ import { makeStyles } from "@material-ui/core/styles";
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+    marginTop: "1rem",
     marginLeft: "1rem",
     marginRight: "1rem",
     "& > *": {
@@ -20,7 +21,7 @@ const useStyles = makeStyles((theme) => ({
   tagItem: {
     display: "flex",
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "left",
   },
 }));
 
@@ -34,14 +35,17 @@ const ProductList = () => {
   const [isError, setIsError] = useState(false);
 
   const products_url = `https://5m6exoj3o7.execute-api.eu-west-1.amazonaws.com/prod/items?tag=${tagValue}`;
+  console.log("Product Url initial");
+  console.log(products_url);
   const tags_url = `https://5m6exoj3o7.execute-api.eu-west-1.amazonaws.com/prod/tags`;
 
-  const handleClick = () => {
-    console.info("You clicked the Chip.");
+  const handleClick = (val) => {
+    console.info(`You have clicked ${val} from TagItem component`);
+    setTagValue(val);
   };
 
-  const handleDelete = () => {
-    console.info("You clicked the delete Chip.");
+  const handleDelete = (tagValue) => {
+    console.info(`You have clicked ${tagValue} from TagItem component`);
   };
 
   const fetchProducts = async () => {
@@ -60,15 +64,20 @@ const ProductList = () => {
   const fetchTags = async () => {
     try {
       const result = await axios(tags_url);
+      console.log();
       setTags(result.data);
     } catch (error) {
       setIsError(true);
     }
   };
+  console.log("Product Url outside useEffect");
+  console.log(products_url);
 
   useEffect(() => {
+    console.log("useEffect run cuz tag is clicked");
+    console.log("Product Url afterTagClick");
+    console.log(products_url);
     fetchProducts();
-    console.log(tagValue);
   }, [tagValue]);
 
   useEffect(() => {
@@ -84,20 +93,13 @@ const ProductList = () => {
       ) : (
         <div className={classes.root}>
           <h1>Tags</h1>
-          <Grid container spacing={2}>
+          <Grid className={classes.tagItem} container spacing={1}>
             {tags.map((tag) => (
-              <Grid item xs sm={3} md={2} lg={1} key={tag.tag}>
-                <Chip
-                  className={classes.tagItem}
-                  icon={<FaceIcon />}
-                  label={tag.tag}
-                  clickable
-                  color="primary"
-                  onClick={handleClick}
-                  onDelete={handleDelete}
-                  deleteIcon={<DoneIcon />}
-                />
-              </Grid>
+              <TagItem
+                name={tag.tag}
+                handleClick={handleClick}
+                handleDelete={handleDelete}
+              />
             ))}
           </Grid>
         </div>
@@ -107,20 +109,11 @@ const ProductList = () => {
         <div>Getting products...</div>
       ) : (
         <div className={classes.root}>
-          <h1>Tags</h1>
+          <h1>Products</h1>
           <Grid container spacing={2}>
             {products.map((product) => (
               <Grid item xs sm={3} md={2} lg={1} key={product.displayName}>
-                <Chip
-                  className={classes.tagItem}
-                  icon={<FaceIcon />}
-                  label={product.displayName}
-                  clickable
-                  color="primary"
-                  onClick={handleClick}
-                  onDelete={handleDelete}
-                  deleteIcon={<DoneIcon />}
-                />
+                <button>{product.displayName}</button>
               </Grid>
             ))}
           </Grid>
