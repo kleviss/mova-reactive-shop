1092d838b851f8522a7ebc7eec9aae459ae3483b
Klevis Xhyra <klevissxh@gmail.com>
2020-11-29 21:01:19 +0100

    adapted style from Collections

--- src/components/CategoryPage.js
+++ src/components/CategoryPage.js
@@ -3,6 +3,9 @@ import ProductItem from "./ProductItem";
 import Grid from "@material-ui/core/Grid";
 import axios from "axios";
 import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -25,6 +28,21 @@ const useStyles = makeStyles((theme) => ({
     display: "flex",
     alignItems: "center",
     justifyContent: "left",
+  },
+  heroContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6),
+  },
+  heroButtons: {
+    marginTop: theme.spacing(4),
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
+  categoryTitle: {
+    textTransform: "capitalize",
+    fontWeight: "bolder"
   },
 }));
 
@@ -58,14 +76,12 @@ const CategoryPage = ({ match }) => {
     };
 
     fetchCategoryItems();
-  
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
     <Fragment>
-      <h1>Category: {match.params.id}</h1>
       {isError && <div>Something went wrong ... Please reload the page</div>}
       {isEmpty && <div>No items at the moment ... Check back next year :p</div>}
 
@@ -73,20 +89,59 @@ const CategoryPage = ({ match }) => {
         <div>Getting your favorite apparel...</div>
       ) : (
         <div>
-          {" "}
-          <Grid className={classes.tagItem} container spacing={1}>
-            {categoryItems.map((item) => (
-              <ProductItem
-                name={item.displayName}
-                description={item.description}
-                price={item.originalPrice}
-                image={item.picture}
-                id={item.itemId}
-                catId={item.categoryId}
-                sizes={item.availableSizes}
-              />
-            ))}
-          </Grid>
+          <div className={classes.heroContent}>
+            <Container maxWidth="sm">
+              <Typography
+                className={classes.categoryTitle}
+                component="h4"
+                variant="h3"
+                align="center"
+                color="textPrimary"
+                gutterBottom
+              >
+                Category: {match.params.id}
+              </Typography>
+              <Typography
+                variant="h6"
+                align="center"
+                color="textSecondary"
+                paragraph
+              >
+                Something short and leading about the collection below—its
+                contents, the creator, etc. Make it short and sweet, but not too
+                short so folks don&apos;t simply skip over it entirely.
+              </Typography>
+              <div className={classes.heroButtons}>
+                <Grid container spacing={2} justify="center">
+                  <Grid item>
+                    <Button variant="contained" color="primary">
+                      Main call to action
+                    </Button>
+                  </Grid>
+                  <Grid item>
+                    <Button variant="outlined" color="primary">
+                      Secondary action
+                    </Button>
+                  </Grid>
+                </Grid>
+              </div>
+            </Container>
+          </div>
+          <Container className={classes.cardGrid} maxWidth="md">
+            <Grid className={classes.tagItem} container spacing={1}>
+              {categoryItems.map((item) => (
+                <ProductItem
+                  name={item.displayName}
+                  description={item.description}
+                  price={item.originalPrice}
+                  image={item.picture}
+                  id={item.itemId}
+                  catId={item.categoryId}
+                  sizes={item.availableSizes}
+                />
+              ))}
+            </Grid>
+          </Container>
         </div>
       )}
     </Fragment>
