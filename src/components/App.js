76589b45c2fb99d15a5f0755218e6852b977b569
Klevis Xhyra <klevissxh@gmail.com>
2020-11-29 14:41:25 +0100

    include Footer in App

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import AppBar from "../components/AppBar";
 import CollectionPage from "../components/CollectionPage";
 import CategoryPage from "../components/CategoryPage";
+import Footer from "../components/Footer";
 import Home from "./Home";
 
 export default function App() {
@@ -15,6 +16,7 @@ export default function App() {
           <Route path="/collection/:id" exact component={CollectionPage} />
           <Route path="/category/:id" exact component={CategoryPage} />
         </Switch>
+        <Footer />
       </React.Fragment>
     </Router>
   );
