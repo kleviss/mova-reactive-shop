2b75961c37f5e6401f58000837704e1c418ef1f8
Klevis Xhyra <klevissxh@gmail.com>
2020-11-29 22:21:58 +0100

    getting details for each product

--- src/components/ProductPage.js
+++ src/components/ProductPage.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Container from "@material-ui/core/Container";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
 import Carousel from "react-material-ui-carousel";
+import axios from "axios";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -37,6 +38,29 @@ var images = [
 const ProductPage = ({ match }) => {
   const classes = useStyles();
 
+  console.log(match);
+
+  const [productItem, setProductItem] = useState([]);
+
+  const url = `https://5m6exoj3o7.execute-api.eu-west-1.amazonaws.com/prod/items`;
+
+  useEffect(() => {
+    console.log("useEffect has been run");
+
+    const fetchProductItem = async () => {
+      try {
+        const result = await axios(url);
+        setProductItem(result.data);
+        console.log(result.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchProductItem();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
     <div className={classes.root}>
       <CssBaseline />
@@ -55,15 +79,25 @@ const ProductPage = ({ match }) => {
           </Grid>
           <Grid item lg={6} md sm={12} xs={12}>
             <Paper className={classes.paper}>
-              <div>
-                <img
-                  style={{ width: "95%" }}
-                  src={
-                    "https://1pqhh33i8vp3w759l43a3pz1-wpengine.netdna-ssl.com/wp-content/uploads/2011/10/535015w_taslan_shorts_w_navy.jpg"
-                  }
-                  alt="rand"
-                />
-              </div>
+              {productItem.map((product) => (
+                <div>
+                  {" "}
+                  <h1>{product.displayName}</h1>
+                  <h1>
+                    {product.collectionId}/{product.tags[0]}
+                  </h1>
+                  <h1>
+                    {product.originalPrice}/{product.currentPrice}
+                  </h1>
+                  <h2>
+                    Select size:
+                    {product.availableSizes.map((size) => (
+                      <p>{size}</p>
+                    ))}
+                  </h2>
+                  <h3>{product.description}</h3>
+                </div>
+              ))}
             </Paper>
           </Grid>
         </Grid>
