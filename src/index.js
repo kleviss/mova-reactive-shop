0faf6dcb5d973807320834d290f52aa6d880bd4c
Klevis Xhyra <klevissxh@gmail.com>
2020-12-03 11:54:47 +0100

    provided global persisted state to main App component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import reactDebugHooks from "react-debug-hooks";
 import "./styles/index.css";
 import { AppContextProvider } from "./context.js";
 import App from "./components/App";
+
+reactDebugHooks(React);
 
 ReactDOM.render(
   <AppContextProvider>
