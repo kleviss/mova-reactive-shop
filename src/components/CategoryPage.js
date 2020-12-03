9018fa1526c7c1c120e6e525bfe55a11fee3d8a4
Klevis Xhyra <klevissxh@gmail.com>
2020-12-03 12:03:28 +0100

    provided each category item state to global AppContextState

--- src/components/CategoryPage.js
+++ src/components/CategoryPage.js
@@ -6,6 +6,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
+import { useAppContext } from "../context";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -49,7 +50,10 @@ const useStyles = makeStyles((theme) => ({
 const CategoryPage = ({ match }) => {
   const classes = useStyles();
 
-  const [categoryItems, setCategoryItems] = useState([]);
+  const {
+    categoryItems: [categoryItems, setCategoryItems],
+  } = useAppContext();
+  // eslint-disable-next-line no-unused-vars
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
   const [isEmpty, setIsEmpty] = useState(false);
@@ -83,92 +87,92 @@ const CategoryPage = ({ match }) => {
   return (
     <Fragment>
       {isError && <div>Something went wrong ... Please reload the page</div>}
-
+      {/* 
       {isLoading ? (
         <div>Getting your favorite apparel...</div>
-      ) : (
-        <div>
-          <div className={classes.heroContent}>
-            <Container maxWidth="sm">
-              <Typography
-                className={classes.categoryTitle}
-                component="h4"
-                variant="h3"
-                align="center"
-                color="textPrimary"
-                gutterBottom
-              >
-                Category: {match.params.id}
-              </Typography>
-              {isEmpty && (
-                <div>
-                  <Typography
-                    style={{ fontWeight: "bolder" }}
-                    variant="h5"
-                    align="center"
-                    color="textSecondary"
-                    paragraph
-                  >
-                    <Typography>
-                      <hr></hr>
-                    </Typography>
-                    No items at the moment ... Check back next year :p{" "}
-                  </Typography>
+      ) : ( */}
+      <div>
+        <div className={classes.heroContent}>
+          <Container maxWidth="sm">
+            <Typography
+              className={classes.categoryTitle}
+              component="h4"
+              variant="h3"
+              align="center"
+              color="textPrimary"
+              gutterBottom
+            >
+              Category: {match.params.id}
+            </Typography>
+            {isEmpty && (
+              <div>
+                <Typography
+                  style={{ fontWeight: "bolder" }}
+                  variant="h5"
+                  align="center"
+                  color="textSecondary"
+                  paragraph
+                >
                   <Typography>
                     <hr></hr>
                   </Typography>
-                </div>
-              )}
+                  No items at the moment ... Check back next year ;-){" "}
+                </Typography>
+                <Typography>
+                  <hr></hr>
+                </Typography>
+              </div>
+            )}
+            <Typography
+              variant="h6"
+              align="center"
+              color="textSecondary"
+              paragraph
+            >
+              Something short and leading about the collection below—its
+              contents, the creator, etc. Make it short and sweet, but not too
+              short so folks don&apos;t simply skip over it entirely.
+            </Typography>
+            <div className={classes.heroButtons}>
+              <Grid container spacing={2} justify="center">
+                <Grid item>
+                  <Button variant="contained" color="primary">
+                    Main call to action
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button variant="outlined" color="primary">
+                    Secondary action
+                  </Button>
+                </Grid>
+              </Grid>
               <Typography
                 variant="h6"
                 align="center"
                 color="textSecondary"
                 paragraph
-              >
-                Something short and leading about the collection below—its
-                contents, the creator, etc. Make it short and sweet, but not too
-                short so folks don&apos;t simply skip over it entirely.
-              </Typography>
-              <div className={classes.heroButtons}>
-                <Grid container spacing={2} justify="center">
-                  <Grid item>
-                    <Button variant="contained" color="primary">
-                      Main call to action
-                    </Button>
-                  </Grid>
-                  <Grid item>
-                    <Button variant="outlined" color="primary">
-                      Secondary action
-                    </Button>
-                  </Grid>
-                </Grid>
-                <Typography
-                  variant="h6"
-                  align="center"
-                  color="textSecondary"
-                  paragraph
-                ></Typography>
-              </div>
-            </Container>
-          </div>
-          <Container className={classes.cardGrid} maxWidth="md">
-            <Grid className={classes.tagItem} container spacing={1}>
-              {categoryItems.map((item) => (
-                <ProductItem
-                  name={item.displayName}
-                  description={item.description}
-                  originalPrice={item.originalPrice}
-                  currentPrice={item.currentPrice}
-                  image={item.picture}
-                  id={item.itemId}
-                  catId={item.categoryId}
-                  sizes={item.availableSizes}
-                />
-              ))}
-            </Grid>
+              ></Typography>
+            </div>
           </Container>
         </div>
-      )}
+        <Container className={classes.cardGrid} maxWidth="md">
+          <Grid className={classes.tagItem} container spacing={1}>
+            {categoryItems.map((item) => (
+              <ProductItem
+                name={item.displayName}
+                description={item.description}
+                originalPrice={item.originalPrice}
+                currentPrice={item.currentPrice}
+                image={item.picture}
+                id={item.itemId}
+                catId={item.categoryId}
+                sizes={item.availableSizes}
+              />
+            ))}
+          </Grid>
+        </Container>
+      </div>
+      {/* )} */}
     </Fragment>
   );
 };
