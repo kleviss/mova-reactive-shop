b0bb445b5818b748079685c63170078e4386c6fc
Klevis Xhyra <klevissxh@gmail.com>
2020-12-04 23:23:00 +0100

    created sample product items for frontend

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -2,8 +2,7 @@ import React from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
-import OrderSummaryItem from "./OrderSummaryItem";
-import ShoppingCartItem from "./ShoppingCartItem";
+
 
 export default function ShoppingCart() {
   return (
@@ -14,15 +13,13 @@ export default function ShoppingCart() {
           <Grid item xs={12} sm={6} md={7} lg={7}>
             <Grid container>
               <Grid item xs>
-                <ShoppingCartItem />
-                <ShoppingCartItem />
-                <ShoppingCartItem />
+               
               </Grid>
             </Grid>
           </Grid>
           <Grid item xs={12} sm={6} md={5} lg={5}>
             <Grid item xs elevation={3}>
-              <OrderSummaryItem />
+
             </Grid>
           </Grid>
         </Grid>
